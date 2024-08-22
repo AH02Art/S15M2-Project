63860d6bbcb9c08322859b679205e1f2377ee96b
Alex Handley <smithvillescholarsah@gmail.com>
2024-08-21 19:32:28 -0500

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -58,7 +58,7 @@ function findBy(filter) {
   return db("users")
     .join("roles", "users.role_id", "roles.role_id")
     .select("user_id", "username", "password", "role_name")
-    .where("user_id", filter);
+    .where(filter);
 }
 
 function findById(user_id) {
@@ -72,6 +72,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db("users")
+    .join("roles", "users.role_id", "roles.role_id")
+    .select("user_id", "username", "password", "role_name")
+    .where("users.user_id", user_id).first();
 }
 
 /**
